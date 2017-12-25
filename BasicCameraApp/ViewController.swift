a694f8fa31ae14481949d864d0eed77418c73bb4
Aritro Paul <aritro.paul61@gmail.com>
2017-12-25 22:04:49 +0530

    Add files via upload

--- BasicCameraApp/ViewController.swift
+++ BasicCameraApp/ViewController.swift
@@ -7,12 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
+    @IBOutlet weak var messageLabelBackground: UIView!
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+    let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            previewView.layer.addSublayer(videoPreviewLayer!)
+            
+            captureSession?.startRunning()
+            previewView.bringSubview(toFront: messageLabel)
+            previewView.bringSubview(toFront: messageLabelBackground)
+            messageLabel.text = "No QR Code Detected"
+            
+            qrCodeFrameView = UIView()
+            if let qrCodeFrameView = qrCodeFrameView{
+                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+                qrCodeFrameView.layer.borderWidth = 2
+                previewView.addSubview(qrCodeFrameView)
+                previewView.bringSubview(toFront: qrCodeFrameView)
+            }
+            
+            func metadataOutput(_output: AVCaptureMetadataOutput, didOutput metadataObjects :[ AVMetadataObject], from connection: AVCaptureConnection){
+                if (metadataObjects.count == 0) {
+                    self.qrCodeFrameView?.frame = CGRect.zero
+                    self.messageLabel.text = "No QR Code Detected"
+                    return
+                }
+                let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+                if metadataObj.type == AVMetadataObject.ObjectType.qr{
+                    let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+                    qrCodeFrameView?.frame = barCodeObject!.bounds
+                    
+                    if metadataObj.stringValue != nil {
+                        messageLabel.text = metadataObj.stringValue
+                    }
+                }
+            }
+            
+        }catch{
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
