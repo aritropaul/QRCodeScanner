a023ae2dc8fb96147f9f12c5b87908d0ce7cfa54
Aritro Paul <aritro.paul61@gmail.com>
2017-12-25 19:54:06 +0530

    Initial Commit

---
+++ BasicCameraApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicCameraApp
+//
+//  Created by Aritro Paul on 25/12/17.
+//  Copyright © 2017 NotACoder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
